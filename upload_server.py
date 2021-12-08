4e2bb5d7ee9a62f4bbd0146ffd65b345284c440a
Richard Shea <rshea@thecubagroup.com>
2021-12-08 14:05:09 +1300

    Remove some debugging stuff

--- upload_server.py
+++ upload_server.py
@@ -4,11 +4,8 @@ from pathlib import PurePosixPath
 import os
 from tempfile import NamedTemporaryFile, TemporaryDirectory, gettempdir
 from flask import Flask, render_template, request
-#from werkzeug import secure_filename
 app = Flask(__name__)
 
-def secure_filename(fname):
-    return fname
 
 @app.route('/upload')
 def upload_file():
@@ -25,22 +22,11 @@ def uploader_file():
         image = request.files.get('file')
         image.name = image.filename
         image = BufferedReader(image)
-        import pprint
-        print(pprint.pprint(dir(image)))
         output_path = os.path.join(gettempdir(), 'r-n-c-b-testing-' + str(time.time_ns()) )
         print("About to write to " + output_path)
         with open(output_path, 'wb') as output_file:
             output_file.write(image.read())
 
-        '''
-        f = request.files['file']
-        print(type(f))
-        ff = NamedTemporaryFile(delete=False, prefix='r-n-c-b-testing-')
-        import pprint
-        print(pprint.pprint(dir(ff)))
-        print(ff.name)
-        f.save(secure_filename(f.name))
-        '''
         return 'file uploaded successfully'
 		
 if __name__ == '__main__':
